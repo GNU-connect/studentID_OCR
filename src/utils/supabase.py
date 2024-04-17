1ce5f6a2224b9d3ec8d77a6a58fa9430caa70c02
Jang-DongHo <donghoo18@naver.com>
2024-04-17 14:08:31 +0900

    [Fix] 젠킨스, 도커 컴포즈 수정

--- src/utils/supabase.py
+++ src/utils/supabase.py
@@ -3,11 +3,8 @@ from os.path import join, dirname
 from supabase import create_client, Client
 from dotenv import load_dotenv
 
-def supabase():
-    url: str = os.getenv("SUPABASE_URL")
-    key: str = os.getenv("SUPABASE_KEY")
-    supabase: Client = create_client(url, key)
-    return supabase
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 def supabase():
     url: str = os.getenv("SUPABASE_URL")
