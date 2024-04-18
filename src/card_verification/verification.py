e67f2f23669b4adfa076abd59559b41b2437273a
Jang-DongHo <donghoo18@naver.com>
2024-04-18 21:11:43 +0900

    [Feat] card verification fixed

--- src/card_verification/verification.py
+++ src/card_verification/verification.py
@@ -32,11 +32,11 @@ model = create_feature_extractor(model, return_nodes={'avgpool': 'avgpool'})
 model.eval()
 
 # test 이미지 저장
-#drive_file_url = os.environ['CARD_VARIFICATION_IMAGE_URL']
-#local_file_path = "temp/test.jpg"
-#if not os.path.exists(local_file_path):
-#    urllib.request.urlretrieve(drive_file_url, local_file_path)
-#    print("test.jpg 파일을 다운로드 받았습니다.")
+drive_file_url = os.environ['CARD_VARIFICATION_IMAGE_URL']
+local_file_path = "temp/test.jpg"
+if not os.path.exists(local_file_path):
+    urllib.request.urlretrieve(drive_file_url, local_file_path)
+    print("test.jpg 파일을 다운로드 받았습니다.")
 
 def img_ocr(img):
     custom_configs=[r'--oem 1 --psm 4',r'--oem 3 --psm 6',r'--oem 1 --psm 3']
